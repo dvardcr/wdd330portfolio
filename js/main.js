e3ab869b8bcfaeb249e943279d9b4b79ea9c66a8
Diego Vargas <68403877+dvardcr@users.noreply.github.com>
2022-01-05 22:35:00 -0600

    Update main.js
    Added code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+var li = document.createElement('li'),
+        anchor = document.createElement('a');
+        anchor.href = a.url;
+        anchor.text = a.name;
+        li.appendChild(anchor);
+        ul.appendChild(li);
